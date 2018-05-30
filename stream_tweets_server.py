463ab21d761f48a9a47d485ed811a1234e7f1f00
Alex Ott <alex.ott@datastax.com>
2018-05-30 14:47:18 +0200

    Rewrite script
    Now it has following functionality:
    - output data to multiple connections
    - support for sampled stream
    - configuration via command-line & environment variables
    - allow to specify languages to filter

--- stream_tweets_server.py
+++ stream_tweets_server.py
@@ -7,126 +7,176 @@ import sys
 import json
 from textblob import TextBlob
 from time import gmtime, strftime
+import argparse
+import traceback
+import os
 
-#This is a basic listener that just prints received tweets to stdout.
+def errprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+    
+# This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
+    def __init__(self, handler):
+        super().__init__()
+        self.handler = handler
 
-    tweet_list = []
-
-    def on_data(self, data):
-        self.tweet_list.append(data)
-        if len(self.tweet_list) >= 10:
-            self.process_tweet_list()
+    def on_status(self, status):
+        self.handler.process_tweet_list(status)
         return True
 
-    def process_tweet_list(self):
-        ImportToCassandra().process_tweet_list(self.tweet_list)
-        self.tweet_list = []
-
     def on_error(self, status):
-        print status
+        print(status)
 
-class ImportToCassandra:
-    def process_tweet_list(self, list):
+class ServerHandler:
+    def __init__(self, args):
+        self.args = args
+        self.connections = []
+    
+    def startTwitter(self):
+        #Variables that contains the user credentials to access Twitter API
+        access_token = self.args.access_token or os.environ.get('TWITTER_ACCESS_TOKEN')
+        access_token_secret = self.args.access_secret or os.environ.get('TWITTER_ACCESS_SECRET')
+        consumer_key = self.args.consumer_key or os.environ.get('TWITTER_CONSUMER_KEY')
+        consumer_secret = self.args.consumer_secret or os.environ.get('TWITTER_CONSUMER_SECRET')
+        if access_token_secret is None or access_token is None or consumer_key is None or consumer_secret is None:
+            print("Access token/secret or consumer key/secret isn't specified!")
+            return False
+        
+        #This handles Twitter authetification and the connection to Twitter Streaming API
+        l = StdOutListener(self)
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        self.stream = Stream(auth, l)
+        
+        errprint("Successfully logged into Twitter and listening..")
+        languages = self.args.languages
+        if (languages is not None):
+            languages = languages.split(',')
+        if self.args.mode == 'terms':
+            terms = self.args.terms.split(',')
+            errprint("Going to listen for following terms: " + str(terms))
+            self.stream.filter(track=terms, async=True, languages=languages)
+        else:
+            errprint("Going to listen for sampled stream")
+            self.stream.sample(async=True, languages=languages)
 
-        for data in (list):
-                try:
-                    data = json.loads(data)
-                    user_id = data['id']
-                    user_name = data['user']['screen_name']
-                    tweet = data['text']
-		    #create_date = data['created_at']
-		    create_date = strftime("%Y-%m-%d %H:%M:%S+0000", gmtime())
-                   
-		    mentions = ""
- 
-		    try:
-			location = data['user']['location']
-		    	in_reply_to = data['in_reply_to_screen_name'].replace('None','')
+        return True
 
-			for doc in data['entities']['user_mentions']:
-				mentions = mentions + doc['screen_name'] + " "
-		    except:
-			location = ""
-			in_reply_to = ""
-			mentions = ""
- 	
-		    twt = "" 
-		    tweet = tweet.replace('\n', '. ').replace('\r', '').replace('|', '').replace('"','')
-		    blob = TextBlob(tweet)		    
+    def startServer(self):
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	 	    for sentence in blob.sentences:
+        # Bind the socket to the port
+        server_address = (self.args.address, self.args.port)
+        errprint('starting up on %s port %s' % server_address)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(server_address)
 
-			twt = twt + str(sentence)+"@@^^"+str(sentence.sentiment.polarity)+"@@^%"  
+        # Connect to Twitter
+        if (not self.startTwitter()):
+            print("Can't start Twitter stream!")
+            return
 
-		    if len(twt) > 4:
-			tweet = twt[:-4]
-			
-		    textToStream = str(user_id)+"~|,~"+str(user_name)+"~|,~"+str(tweet)+"~|,~"+str(location)+"~|,~"+str(create_date)+"~|,~"+str(in_reply_to)+"~|,~"+str(mentions.strip())+"~|,~"+"\n"
-		    textToStream = textToStream.lower()
-		    
-		    try :
-    			connection.sendall(textToStream)
-		    except socket.error:
-    		      	print 'Send failed'
-		    
-                    #connection.sendall(textToStream)
-                    print textToStream 
-                except:
-		    print "exception!"
-                    pass
+        # Listen for incoming connections
+        sock.listen(1)
 
-        #session.execute(batch)
-
-class startStreaming:
-    def letsGo(self):
-	if __name__ == '__main__':
-
-	    #Variables that contains the user credentials to access Twitter API
-	    access_token = "<enter your access token here>"
-	    access_token_secret = "<enter your access token secret here>"
-	    consumer_key = "<enter your consumer key here>"
-	    consumer_secret = "<enter your consumer secret here>"
-
-            #This handles Twitter authetification and the connection to Twitter Streaming API
-            l = StdOutListener()
-            auth = OAuthHandler(consumer_key, consumer_secret)
-            auth.set_access_token(access_token, access_token_secret)
-            stream = Stream(auth, l)
-
-            #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-	
-            print "Successfully logged into Twitter and listening.."
-            #stream.filter(track=['couchbase','apache','datamanagement','nosql', 'mongodb', 'datastax', 'bigdata', 'hadoop', 'neo4j', 'titandb'])
-	    stream.filter(track=['tories','labour','brexit','corbyn', 'conservatives', 'ukip', 'libdems', 'trump','farage'])
-	    #stream.filter(track=['ucl','champsleague','championsleague','prem','epl','laliga','seriea','facup','worldcup','premierleague','soccer'])
-
-
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the port
-server_address = ('192.168.0.40', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
-
-# Listen for incoming connections
-sock.listen(1)
-
-while True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
-
-    try:
-        print >>sys.stderr, 'connection from', client_address
-
-        # Receive the data in small chunks and retransmit it
         while True:
-                #connection.sendall(data)
-                startStreaming().letsGo()
+            # Wait for a connection
+            errprint('waiting for a connection')
+            try:
+                connection, client_address = sock.accept()
 
-    finally:
-        # Clean up the connection
-        connection.close()
+                errprint('connection from ' + str(client_address))
+                self.connections.append(connection)
+            except KeyboardInterrupt:
+                errprint("Got Ctrl-C, exiting...")
+                self.stream.disconnect()
+                for connection in self.connections:
+                    connection.close()
+                return
+
+    def process_tweet_list(self, status):
+        try:
+            user_id = status.id
+            user_name = status.user.screen_name
+            tweet = status.text
+            #create_date = data['created_at']
+            # TODO: use data from tweet itself...
+            create_date = strftime("%Y-%m-%d %H:%M:%S+0000", gmtime())
+                
+            mentions = ""
+ 
+            try:
+                location = status.user.location
+                in_reply_to = str(status.in_reply_to_screen_name).replace('None','')
+
+                for doc in status.entities['user_mentions']:
+                    mentions = mentions + doc['screen_name'] + " "
+                mentions.strip()
+            except KeyboardInterrupt:
+                raise
+            except Exception as e:
+                errprint("data exception! " + str(e) + ", data=" + str(status))
+                location = ""
+                in_reply_to = ""
+                mentions = ""
+        
+            twt = "" 
+            tweet = tweet.replace('\n', '. ').replace('\r', '').replace('|', '').replace('"','')
+            blob = TextBlob(tweet)              
+
+            for sentence in blob.sentences:
+                twt = twt + str(sentence)+"@@^^"+str(sentence.sentiment.polarity)+"@@^%"  
+
+            if len(twt) > 4:
+                tweet = twt[:-4]
+                        
+            textToStream = str(user_id)+"~|,~"+str(user_name)+"~|,~"+str(tweet)+"~|,~"+str(location)+"~|,~"+str(create_date)+"~|,~"+str(in_reply_to)+"~|,~"+str(mentions.strip())+"~|,~"+"\n"
+            textToStream = textToStream.lower()
+
+            conLen = len(self.connections)
+            current = 0
+            while current < conLen:
+                connection=self.connections[current]
+                try :
+                    connection.sendall(textToStream.encode('utf-8'))
+                    current = current + 1
+                except KeyboardInterrupt:
+                    raise
+                except socket.error:
+                    errprint('Send failed! Removing this connection from list of connections...')
+                    del self.connections[current]
+                    conLen = conLen - 1
+                    
+            print(textToStream)
+
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            errprint("exception! " + str(e)  + ", data=" + str(status))
+            traceback.print_exc()
+            pass
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", default="terms", choices=['terms','sample'])
+    parser.add_argument("--terms", default=None, type=str,
+                        help="comma-separated list of terms to fetch. Required when --mode is 'terms'")
+    parser.add_argument("--address", default="127.0.0.1", type=str, help = "Address to listen on")
+    parser.add_argument("--port", default=10000, type=int, help="Port to listen on")
+    parser.add_argument("--access-token", type=str, dest='access_token', help="Twitter's access token")
+    parser.add_argument("--access-secret", type=str, dest='access_secret', help="Twitter's access token secret")
+    parser.add_argument("--consumer-key", type=str, dest='consumer_key', help="Twitter's consumer key")
+    parser.add_argument("--consumer-secret", type=str, dest='consumer_secret', help="Twitter's consumer secret")
+    parser.add_argument("--languages", type=str, help="Comma-separated list of languages to handle")
+
+    args = parser.parse_args()
+    if args.mode == 'terms' and args.terms is None:
+        errprint("--terms is required when 'terms' mode is used!")
+        parser.print_help()
+        exit(1)
+    handler = ServerHandler(args)
+    handler.startServer()
+
+if __name__ == "__main__":
+    main()
